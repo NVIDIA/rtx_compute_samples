25f670c5cb66b184f4ade6dc967155330641f5a5
Vishal Mehta <vishalm@nvidia.com>
2021-09-27 14:23:04 +0200

    add folder for materials

---
+++ optixMaterials/src/optixMaterials.cpp
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of NVIDIA CORPORATION nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <iostream>
+
+#include "common/common.h"
+
+#include "maxBounce.h"
+#include "params.hpp"
+#include "rtxFunctions.hpp"
+
+RTXDataHolder *rtx_dataholder;
+
+// small problem for debugging
+uint32_t width = 8u;  // buffer size x
+uint32_t height = 8u; // buffer size y
+uint32_t depth = 8u;
+
+// fits on a single screen
+// uint32_t height = 32u; // buffer size y
+// uint32_t width = 32u;  // buffer size x
+
+// 100M rays
+// uint32_t  height = 10000u;   // buffer size y
+// uint32_t  width  = 10000u;   // buffer size x
+
+int main(int argc, char **argv) {
+  std::string testfile = "sphereAndPlane";
+  std::string obj_file = OBJ_DIR "./" + testfile + ".obj";
+  std::cout << "Mesh file = " << obj_file << std::endl;
+  std::string ptx_filename = BUILD_DIR "/ptx/optixPrograms.ptx";
+
+  cudaStream_t stream;
+  CUDA_CHECK(cudaStreamCreate(&stream));
+
+  rtx_dataholder = new RTXDataHolder();
+  std::cout << "Initializing Context \n";
+  rtx_dataholder->initContext();
+  std::cout << "Reading PTX file and creating modules \n";
+  rtx_dataholder->createModule(ptx_filename);
+  std::cout << "Creating Optix Program Groups \n";
+  rtx_dataholder->createProgramGroups();
+  std::cout << "Linking Pipeline \n";
+  rtx_dataholder->linkPipeline();
+  std::cout << "Building Shader Binding Table (SBT) \n";
+  rtx_dataholder->buildSBT();
+
+  std::vector<float3> vertices;
+  std::vector<uint3> triangles;
+  std::cout << "Building Acceleration Structure \n";
+  OptixAabb aabb_box =
+      rtx_dataholder->buildAccelerationStructure(obj_file, vertices, triangles);
+
+  // calculate delta
+  float3 delta = make_float3((aabb_box.maxX - aabb_box.minX) / width,
+                             (aabb_box.maxY - aabb_box.minY) / height,
+                             (aabb_box.maxZ - aabb_box.minZ) / depth);
+  float3 min_corner = make_float3(aabb_box.minX, aabb_box.minY, aabb_box.minZ);
+
+  float *d_tpath;
+  CUDA_CHECK(cudaMalloc((void **)&d_tpath, width * height * sizeof(float)));
+
+  // Algorithmic parameters and data pointers used in GPU program
+  Params params;
+  params.min_corner = min_corner;
+  params.delta = delta;
+  params.handle = rtx_dataholder->gas_handle;
+  params.width = width;
+  params.height = height;
+  params.depth = depth;
+  params.tpath = d_tpath;
+
+  Params *d_param;
+  CUDA_CHECK(cudaMalloc((void **)&d_param, sizeof(Params)));
+  CUDA_CHECK(
+      cudaMemcpy(d_param, &params, sizeof(params), cudaMemcpyHostToDevice));
+
+  const OptixShaderBindingTable &sbt = rtx_dataholder->sbt;
+
+  std::cout << "Launching Ray Tracer to scatter rays \n";
+  OPTIX_CHECK(optixLaunch(rtx_dataholder->pipeline, stream,
+                          reinterpret_cast<CUdeviceptr>(d_param),
+                          sizeof(Params), &sbt, width, height, 1));
+
+  CUDA_CHECK(cudaDeviceSynchronize());
+
+  std::cout << "Cleaning up ... \n";
+  CUDA_CHECK(cudaFree(d_tpath));
+  CUDA_CHECK(cudaFree(d_param));
+  delete rtx_dataholder;
+
+  return 0;
+}
---
+++ optixMaterials/src/optixPrograms.cu
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of NVIDIA CORPORATION nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "common/vec_func.cuh"
+#include "params.hpp"
+#include <optix.h>
+
+extern "C" static __constant__ Params params;
+
+struct Payload {
+  unsigned int ray_id; // unique id of the ray
+  float tpath;         // total lenth of the path with multiple bounces
+};
+
+extern "C" __global__ void __raygen__prog() {
+  const uint3 launch_index = optixGetLaunchIndex();
+  const uint3 launch_dim = optixGetLaunchDimensions();
+
+  const float3 &min_corner = params.min_corner;
+  const float3 &delta = params.delta;
+
+  float xo = min_corner.x + delta.x * launch_index.x;
+  float yo = min_corner.y + delta.y * launch_index.y;
+  float zo = min_corner.z;
+
+  // setting the per ray data (payload)
+  Payload pld;
+  pld.tpath = 0.0f;
+  pld.ray_id = launch_index.x + launch_dim.x * launch_index.y;
+
+  // create a ray
+  float3 ray_origin = make_float3(xo, yo, zo);
+  float3 ray_direction = normalize(make_float3(0.0, 0.0, 1.0));
+
+  float tmin = 0.0f;
+  float tmax = delta.z + 100.0;
+  float ray_time = 0.0f;
+  OptixVisibilityMask visibilityMask = 255;
+  unsigned int rayFlags = OPTIX_RAY_FLAG_DISABLE_ANYHIT;
+  unsigned int SBToffset = 0;
+  unsigned int SBTstride = 0;
+  unsigned int missSBTIndex = 0;
+
+  // Extract Payload as unsigned int
+  unsigned int ray_id_payload = pld.ray_id;
+  unsigned int tpath_payload = __float_as_uint(pld.tpath);
+
+  optixTrace(params.handle, ray_origin, ray_direction, tmin, tmax, ray_time,
+             visibilityMask, rayFlags, SBToffset, SBTstride, missSBTIndex,
+             ray_id_payload, tpath_payload);
+
+  // Store back paylaod to the Payload struct
+  pld.ray_id = ray_id_payload;
+  pld.tpath = __uint_as_float(tpath_payload);
+
+  // store the result and  number of bounces back to the global buffers
+  params.tpath[pld.ray_id] = pld.tpath;
+}
+
+extern "C" __global__ void __closesthit__prog() {
+  unsigned int tri_id = optixGetPrimitiveIndex();
+  // We defined out geometry as a triangle geometry. In this case the
+  // We add the t value of the intersection
+  float ray_tmax = optixGetRayTmax();
+
+  unsigned int ray_id_payload = optixGetPayload_0();
+
+}
+
+// extern "C" __global__ void __miss__prog() {}
+// extern "C" __global__ void __anyhit__prog() {}
